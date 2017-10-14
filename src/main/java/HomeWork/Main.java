f26b09c2d7c982f80dda3727a5a9d1bac2f5fc4d
Aibar-B <aibro_85@mail.ru>
2017-10-14 09:13:01 +0600

    Initial commit

---
+++ src/main/java/HomeWork/Main.java
@@ -0,0 +1,34 @@
+package HomeWork;
+
+public class Main {
+    public static void main(String[] args) {
+
+        boolean number1 = isItInAscendingOrder(123456);
+        boolean number2 = isItInAscendingOrder(6153);
+        System.out.println(number1);
+        System.out.println(number2);
+
+    }
+
+    public static boolean isItInAscendingOrder(int a) {
+
+        String s = String.valueOf(a);
+        String[] m = new String[s.length()];
+
+        for (int i = 0; i <s.length() ; i++) {
+            m [i] = String.valueOf(s.charAt(i));
+        }
+
+        boolean res = true;
+        for (int i = 0; i < m.length - 1; i++) {
+            if (Integer.parseInt(m[i]) > Integer.parseInt(m[i + 1])) {
+                res = false;
+            } else {
+                continue;
+            }
+        }
+        return res;
+    }
+
+
+}
